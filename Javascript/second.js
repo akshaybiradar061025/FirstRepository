9990c840af2cf9e3fd7f22ed0976b05c97f27185
akshaybiradar061025 <Akshay.B@revappayyaitservices.com>
2025-10-13 19:11:43 +0530

    files to push

---
+++ Javascript/second.js
@@ -0,0 +1,81 @@
+/* Uppercase Conversion */
+// Take a string "hello world" and print it in uppercase
+
+// let str = "hello world";
+// console.log(str.toUpperCase());
+
+
+/* Lowercase Conversion */
+// Take a string "JAVASCRIPT" and print it in lowercase
+
+// let str = "JAVASCRIPT";
+// console.log(str.toLowerCase());
+
+
+/* Find Word Position */
+// Find the position (index) of "world" in the string "Hello world" using indexOf()
+
+// let text = "Hello world";
+// console.log(text.indexOf("world"));
+
+
+/* Extract Substring */
+// From "JavaScript" ,extract and print "Script" using slice().
+
+// let text = "JavaScript";
+// console.log(text.slice(4));
+
+
+/* Replace a Word */
+// Replace "John" with "Avinash" in the string "My name is John".
+
+// let str = "My name is John";
+// let newstr = str.replace("John" , "Avinash" );
+// console.log(newstr);
+
+
+/* Count String Length */
+// Print the total number of characters in "I love coding" (use.length).
+
+// let str = "I love coding";
+// console.log(str.length);
+
+
+/* Check Start or End Word */
+// Check if a string "Hello JavaScript" starts with "Hello" and ends with "Script".
+
+// let str = "Hello JavaScript";
+// console.log(str.startsWith("Hello"));
+// console.log(str.endsWith("Script"));
+
+
+/* Find Word in Sentence */
+// Write a function that checks if a given word exists in a sentence(use includes()).
+// Input--"My name is Avinash", "Avinash"
+// Output-- true
+// ?????
+
+/* Combine Two Strings */
+// Combine "Hello" and "World" using template literals to get "Hello World".
+// ?????
+
+/* Reverse a String (using loop or methods)*/
+// Write a function that takes "Avinash" and returns "hsanivA".
+// ?????
+
+
+// let ans = " ";
+// for(let i = 1; i <= 10; i++){
+//     ans = ans + i + " ";
+// }
+// console.log(ans);
+
+
+let ans = " ";
+for(let i = 1; i <= 10; i++){
+    ans = ans + i + " ";
+}
+for(let j =1; j <=10; j++){
+    console.log(ans);
+}
+
